8d8a7fb4e1068a5918b1e136d57337526eab8fd0
Jeysson Guevara <guevara@civicplus.com>
2023-03-03 18:43:27 -0500

    Added cleanup code on destroy.

--- src/runtime.js
+++ src/runtime.js
@@ -283,6 +283,15 @@ cr.behaviors.SimpleThree_Sprite = function (runtime) {
         // called when associated object is being destroyed
         // note runtime may keep the object and behavior alive after this call for recycling;
         // release, recycle or reset any references here as necessary
+        if(this.mesh !== undefined){
+            this.pivot?.remove(this.mesh);
+        }
+        this.simpleThree?.scene?.remove(this.pivot);
+
+        this.currentGeometry = undefined;
+        this.currentGeometry = undefined;
+        this.mesh = undefined;
+        this.pivot = undefined;
     };
 
     // called when saving the full state of the game
